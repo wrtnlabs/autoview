6d872a0b16fe1ff2f31466ec8c3c5e937a5c2f45
Nikki <nikki@Nikkiui-MacBookPro.local>
2025-03-05 15:33:04 +0900

    chore: init @autoview/ui

---
+++ packages/interface/src/data-display/index.ts
@@ -0,0 +1,5 @@
+export * from "./badge";
+export * from "./chip";
+export * from "./divider";
+export * from "./stats";
+export * from "./tag";
--- packages/interface/src/index.ts
+++ packages/interface/src/index.ts
@@ -1,2 +1,3 @@
+export * from "./data-display";
 export * from "./properties/IAutoViewComponentProps";
 export * from "./properties/IAutoViewComponentPropsBase";
