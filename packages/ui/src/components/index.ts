e56739ecab7af6e3ad92df9b4c3e0647fab038a7
Nikki <nikki@Nikkiui-MacBookPro.local>
2025-03-10 14:52:50 +0900

    chore: export remaining components

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,7 @@
 export * from "./badge";
 export * from "./chip";
+export * from "./carousel";
+export * from "./icon";
 export * from "./image-avatar";
 export * from "./letter-avatar";
 export * from "./typography";
