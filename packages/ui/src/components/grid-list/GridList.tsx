89269929fde21a4c0632e45749b05a2b402638ba
Nikki <nikki@Nikkiui-MacBookPro.local>
2025-03-13 15:00:59 +0900

    feat(grid-list): implement GridList

--- packages/ui/src/components/grid-list/GridList.tsx
+++ packages/ui/src/components/grid-list/GridList.tsx
@@ -1,5 +1,16 @@
 import { IAutoViewGridListProps } from "@autoview/interface";
+import { Grid2 as MuiGrid } from "@mui/material";
+import React from "react";
+
+import { renderComponent } from "../../renderer";
+import { transformGridListProps } from "./transform";
 
 export const GridList = (props: IAutoViewGridListProps) => {
-  return null;
+  const listProps = transformGridListProps(props);
+
+  return (
+    <MuiGrid container {...listProps}>
+      {props.items.map((item) => renderComponent(item.children))}
+    </MuiGrid>
+  );
 };
