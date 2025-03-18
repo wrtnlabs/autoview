52baf04c997e87d520cca3e4dbcf210686161fd7
AcrylicShrimp <led789zxpp@naver.com>
2025-03-18 13:33:30 +0900

    feat: add methods to worker too

--- packages/compiler/src/worker/AutoViewCompilerService.ts
+++ packages/compiler/src/worker/AutoViewCompilerService.ts
@@ -13,9 +13,21 @@ export class AutoViewCompilerService implements IAutoViewCompilerService {
     this.compiler_ = new AutoViewCompiler(props);
   }
 
+  public generateBoilerplate(): string {
+    if (this.compiler_ === null)
+      throw new Error("You have not initialized yet.");
+    return this.compiler_.generateBoilerplate();
+  }
+
   public async compile(script: string): Promise<IAutoViewCompilerResult> {
     if (this.compiler_ === null)
       throw new Error("You have not initialized yet.");
     return this.compiler_.compile(script);
+  }
+
+  public async compileRandom(): Promise<IAutoViewCompilerResult> {
+    if (this.compiler_ === null)
+      throw new Error("You have not initialized yet.");
+    return this.compiler_.compileRandom();
   }
 }
