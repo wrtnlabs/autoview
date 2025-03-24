dfbe257b500bb74737a99427149d07f2ced9083c
Amber <hyeyoenchurros@naver.com>
2025-03-24 12:49:29 +0900

    fix: spell

--- website/app/_constants/landing.ts
+++ website/app/_constants/landing.ts
@@ -2,16 +2,16 @@ export const STEPPER_CONTENTS = [
   {
     title: "Upload",
     description:
-      "Select schema presets or uplaod your own schma as Swagger file format. It might takes 1 minutes for transmiting data for UI format.",
+      "Select schema presets or upload your own schema as Swagger file format. It might takes 1 minutes for transmitting data for UI format.",
   },
   {
     title: "Converting",
     description:
-      "Select schema presets or uplaod your own schma as Swagger file format. It might takes 1 minutes for transmiting data for UI format.",
+      "Select schema presets or upload your own schema as Swagger file format. It might takes 1 minutes for transmitting data for UI format.",
   },
   {
     title: "Display",
     description:
-      "Select schema presets or uplaod your own schma as Swagger file format. It might takes 1 minutes for transmiting data for UI format.",
+      "Select schema presets or upload your own schema as Swagger file format. It might takes 1 minutes for transmitting data for UI format.",
   },
 ];
