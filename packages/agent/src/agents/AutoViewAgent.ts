268ac29bcc968b34af7ecbf343c6df88de549b31
AcrylicShrimp <led789zxpp@naver.com>
2025-03-27 16:30:28 +0900

    refactor: rename

--- packages/agent/src/agents/AutoViewAgent.ts
+++ packages/agent/src/agents/AutoViewAgent.ts
@@ -1,169 +1,147 @@
-import OpenAI from "openai";
-import typia, { ILlmFunctionOfValidate, IValidation } from "typia";
+import { IAutoViewComponentProps } from "@autoview/interface";
+import { ChatGptTypeChecker } from "@samchon/openapi";
+import typia from "typia";
 
-import { IAutoViewApplication } from "../structures/IAutoViewApplication";
-import { IAutoViewValueResult } from "../structures/IAutoViewValueResult";
-import { IAutoViewAgentProvider } from "../structures/agents/IAutoViewAgentProvider";
-import { IAutoViewAgentValueProps } from "../structures/agents/IAutoViewAgentValueProps";
+import { CodeGeneration, PlanGeneration } from "../passes";
+import { IComponentWithoutValueValidator } from "../passes/common";
+import { IAutoViewAgentProvider } from "../structures";
 
 export namespace AutoViewAgent {
-  export const value = (
-    props: IAutoViewAgentValueProps,
-  ): Promise<IAutoViewValueResult> =>
-    execute({
-      provider: props.provider,
-      retry: props.retry ?? 3,
-      prompt: [
-        "I wanna render this JSON value into a display components.",
-        "",
-        "Please fill the proper arguments of the render() function",
-        "with the given properties.",
-        "",
-        "For reference, the `value` property is the actual value to render,",
-        "and the others are metadata describing the value like JSON schema.",
-        "By the way, as the other properties are all optional, it is possible",
-        "to being only the `value` property existing case.",
-        "",
-        "```json",
-        JSON.stringify(props.inputs),
-        "```",
-      ].join("\n"),
+  export interface IResult {
+    /**
+     * (Intermediate reasoning) The plan of the visualization.
+     */
+    visualizationPlanning: string;
+    /**
+     * (Intermediate reasoning) React-like component plan.
+     */
+    componentPlan: string;
+    /**
+     * (Intermediate reasoning) The analysis of the input schema and data exploration.
+     */
+    analysis: string;
+
+    /**
+     * The transform function that takes input value (which follows the input schema) and returns the output value (which follows the {@link IAutoViewComponentProps}).
+     */
+    // transform: Function;
+
+    /**
+     * The random function that generates random data which follows the {@link IAutoViewComponentProps}.
+     *
+     * It is useful to quickly test your generated component.
+     */
+    // random: Function;
+
+    /**
+     * The TypeScript code of the transform function.
+     *
+     * This code includes several import statements, preventing it to include bunch of DTO interface declarations.
+     */
+    transformTsCode: string;
+  }
+
+  export async function execute(
+    provider: IAutoViewAgentProvider,
+    inputSchema: unknown,
+  ): Promise<IResult> {
+    const components = listComponents();
+
+    const planGenerationAgent = new PlanGeneration.Agent();
+    const codeGenerationAgent = new CodeGeneration.Agent();
+    await codeGenerationAgent.open();
+
+    const plan = await planGenerationAgent.execute({
+      provider,
+      inputSchema,
+      components,
     });
 
-  const execute = async (ctx: {
-    provider: IAutoViewAgentProvider;
-    prompt: string;
-    retry: number;
-  }): Promise<IAutoViewValueResult> => {
-    let result: IAutoViewValueResult | null = null;
     try {
-      while (true) {
-        result = await process({
-          provider: ctx.provider,
-          prompt: ctx.prompt,
-          failure: result?.type === "invalid" ? result : null,
-        });
-        if (--ctx.retry <= 0 || result?.type === "success") break;
+      const { analysis, transformTsCode } = await codeGenerationAgent.execute({
+        provider,
+        inputSchema,
+        componentSchema: componentSchema(),
+        componentPlan: plan.component,
+      });
+
+      return {
+        visualizationPlanning: plan.visualizationPlanning,
+        componentPlan: plan.component,
+        analysis,
+        // transform,
+        // random,
+        transformTsCode,
+      };
+    } finally {
+      try {
+        await codeGenerationAgent.close();
+      } catch (error) {
+        console.warn(`failed to close code generation agent: ${error}`);
       }
-      return result;
-    } catch (error) {
-      return {
-        type: "error",
-        error: error as Error,
-      };
     }
-  };
+  }
 
-  const process = async (ctx: {
-    provider: IAutoViewAgentProvider;
-    prompt: string;
-    failure: IAutoViewValueResult.IInvalid | null;
-  }): Promise<IAutoViewValueResult> => {
-    const completion: OpenAI.ChatCompletion =
-      await ctx.provider.api.chat.completions.create(
-        {
-          model: ctx.provider.model,
-          tools: [
-            {
-              type: "function",
-              function: {
-                name: FUNCTION.name,
-                description: FUNCTION.description,
-                parameters: FUNCTION.parameters as Record<string, any>,
-              },
-            },
-          ],
-          messages: [
-            {
-              role: "system",
-              content: [
-                "You are a helpful customer support assistant.",
-                "",
-                "Use the supplied tools to assist the user.",
-              ].join("\n"),
-            },
-            {
-              role: "user",
-              content: ctx.prompt,
-            },
-            ...(ctx.failure
-              ? [
-                  {
-                    role: "assistant",
-                    tool_calls: [
-                      {
-                        type: "function",
-                        id: ctx.failure.id,
-                        function: {
-                          name: FUNCTION.name,
-                          arguments: JSON.stringify(
-                            ctx.failure.validation.data,
-                          ),
-                        },
-                      },
-                    ],
-                  } satisfies OpenAI.ChatCompletionAssistantMessageParam,
-                  {
-                    role: "tool",
-                    tool_call_id: ctx.failure.id,
-                    content: JSON.stringify(ctx.failure.validation.errors),
-                  } satisfies OpenAI.ChatCompletionToolMessageParam,
-                  {
-                    role: "system",
-                    content: [
-                      "You A.I. assistant has composed wrong arguments.",
-                      "",
-                      "Correct it at the next function calling.",
-                    ].join("\n"),
-                  } satisfies OpenAI.ChatCompletionSystemMessageParam,
-                ]
-              : []),
-          ],
-        },
-        ctx.provider.options,
-      );
-    const results: ICallResult[] = completion.choices
-      .map((choice) =>
-        (choice.message.tool_calls ?? [])
-          .filter((call) => call.function.name === FUNCTION.name)
-          .map((call) => ({
-            id: call.id,
-            arguments: FUNCTION.validate(
-              JSON.parse(call.function.arguments),
-            ) as IValidation<IAutoViewApplication.IRenderArgument>,
-          })),
-      )
-      .flat();
-    const success = results.find((r) => r.arguments.success);
-    const failure = results.find((r) => r.arguments.success === false);
-    if (success)
-      return {
-        type: "success",
-        props: success.arguments.success
-          ? success.arguments.data.component
-          : null!,
-        completion,
-      };
-    else if (failure)
-      return {
-        type: "invalid",
-        id: failure.id,
-        validation: failure.arguments as IValidation.IFailure,
-        completion,
-      };
-    else
-      return {
-        type: "nothing",
-        completion,
-      };
-  };
-}
+  function listComponents(): IComponentWithoutValueValidator[] {
+    const components: IComponentWithoutValueValidator[] = [];
 
-const FUNCTION: ILlmFunctionOfValidate<"chatgpt"> = typia.llm
-  .application<IAutoViewApplication, "chatgpt", { reference: true }>()
-  .functions.find((f) => f.name === "render")!;
+    ChatGptTypeChecker.visit({
+      closure(schema) {
+        if (!ChatGptTypeChecker.isReference(schema)) {
+          return;
+        }
 
-interface ICallResult {
-  id: string;
-  arguments: IValidation<IAutoViewApplication.IRenderArgument>;
+        if (
+          !schema.$ref.startsWith("#/$defs/IAutoView") ||
+          !schema.$ref.endsWith("Props") ||
+          schema.$ref.includes(".")
+        ) {
+          return;
+        }
+
+        const name = schema.$ref.split("/").pop();
+
+        if (!name) {
+          return;
+        }
+
+        const definition = PARAMETERS.$defs[name];
+
+        if (!definition) {
+          return;
+        }
+
+        components.push({
+          name,
+          description: definition.description ?? definition.title ?? "",
+          componentSchema: definition as Record<string, unknown>,
+        });
+      },
+      $defs: PARAMETERS.$defs,
+      schema: PARAMETERS.properties["props"]!,
+    });
+
+    return components;
+  }
+
+  function componentSchema(): unknown {
+    if (!ChatGptTypeChecker.isObject(PARAMETERS)) {
+      throw new Error("PARAMETERS is not an object.");
+    }
+
+    return {
+      ...PARAMETERS.properties["props"],
+      $defs: PARAMETERS.$defs,
+    };
+  }
+
+  const PARAMETERS = typia.llm.parameters<
+    {
+      props: IAutoViewComponentProps;
+    },
+    "chatgpt",
+    {
+      reference: true;
+    }
+  >();
 }
