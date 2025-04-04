5296a78dfb9c392a0bc89827164c77773941c6fa
Amber <81298254+syhgpd@users.noreply.github.com>
2025-04-04 15:29:59 +0900

    fix: link (#150)

--- website/app/_components/landing/PreviewSection.tsx
+++ website/app/_components/landing/PreviewSection.tsx
@@ -3,18 +3,20 @@
 import { toast } from "react-toastify/unstyled";
 import { PreviewCard } from "../common/preview-card";
 import { Toast } from "../common/toast";
+import Link from "next/link";
 
 
 export function PreviewSection() {
   return (
     <section className="flex flex-col-reverse md:flex-row md:justify-between w-full pb-[74px] md:pb-[120px]">
-      <PreviewCard
-        href="https://wrtnlabs.io/agentica/"
-        title="Agentica"
-        subtitle="Previous"
-        image="/autoview/agentica.png"
-        direction="left"
-      />
+      <Link href="https://wrtnlabs.io/agentica">
+        <PreviewCard
+          title="Agentica"
+          subtitle="Previous"
+          image="/autoview/agentica.png"
+          direction="left"
+        />
+      </Link>
       <PreviewCard
         subtitle="Next"
         title="Agent OS"
