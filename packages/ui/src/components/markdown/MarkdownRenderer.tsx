f84da5bcd02f3488b90c75173b300047528a854a
Amber <hyeyoenchurros@naver.com>
2025-04-09 18:10:52 +0900

    chore: add code components

--- packages/ui/src/components/markdown/MarkdownRenderer.tsx
+++ packages/ui/src/components/markdown/MarkdownRenderer.tsx
@@ -2,7 +2,6 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { MarkdownComponents } from "./MarkdownComponents";
 
-
 export const MarkdownRenderer = ({ children }: { children: string; }) => {
   return (
     <ReactMarkdown
