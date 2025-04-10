5eb8bf9fa7473d1b67d88f422342e6ef94466ac9
AcrylicShrimp <led789zxpp@naver.com>
2025-04-10 17:30:45 +0900

    feat: improve comment

--- packages/interface/src/components/IAutoViewCarouselProps.ts
+++ packages/interface/src/components/IAutoViewCarouselProps.ts
@@ -7,7 +7,17 @@ import {
 export interface IAutoViewCarouselProps
   extends IAutoViewComponentPropsBase<"Carousel"> {
   autoPlay?: boolean;
+  /**
+   * Duration is not in milliseconds because Embla uses an attraction physics simulation when scrolling instead of easings.
+   *
+   * Only values between `20` and `60` are recommended.
+   */
   interval?: number;
+  /**
+   * Whether the carousel should loop.
+   *
+   * It is recommended to set this to `true` for most cases.
+   */
   infinite?: boolean;
   gutter?: number;
   effect?: "slide" | "fade";
