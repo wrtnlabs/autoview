df22f2249badea6aac786c6583fa310998a9df90
Jeongho Nam <samchon.github@gmail.com>
2025-04-13 22:09:45 +0900

    Bump up `@samchon/openapi` for DeepSeek supporting.

--- packages/agent/src/agents/convert-schema.ts
+++ packages/agent/src/agents/convert-schema.ts
@@ -2,10 +2,8 @@ import {
   IChatGptSchema,
   IClaudeSchema,
   IGeminiSchema,
-  ILlamaSchema,
   ILlmSchema,
   ILlmSchemaV3,
-  ILlmSchemaV3_1,
   OpenApi,
 } from "@samchon/openapi";
 import { LlmSchemaComposer } from "@samchon/openapi/lib/composers/LlmSchemaComposer";
@@ -27,24 +25,17 @@ export function convertSchema<Model extends ILlmSchema.Model>(
         ($defs ?? {}) as unknown as Record<string, IChatGptSchema>,
       );
     case "claude":
+    case "deepseek":
+    case "llama":
+    case "3.1":
       return convertSchemaFromClaudeSchema(
         schema as IClaudeSchema,
         ($defs ?? {}) as unknown as Record<string, IClaudeSchema>,
       );
     case "gemini":
       return convertSchemaFromGeminiSchema(schema as IGeminiSchema);
-    case "llama":
-      return convertSchemaFromLlamaSchema(
-        schema as ILlamaSchema,
-        ($defs ?? {}) as unknown as Record<string, ILlamaSchema>,
-      );
     case "3.0":
       return convertSchemaFromLlmSchemaV3(schema as ILlmSchemaV3);
-    case "3.1":
-      return convertSchemaFromLlmSchemaV3_1(
-        schema as ILlmSchemaV3_1,
-        ($defs ?? {}) as unknown as Record<string, ILlmSchemaV3_1>,
-      );
     default:
       throw new Error(`the model ${model} is unsupported`);
   }
@@ -97,23 +88,6 @@ export function convertSchemaFromGeminiSchema(
   };
 }
 
-export function convertSchemaFromLlamaSchema(
-  schema: ILlamaSchema,
-  $defs: Record<string, ILlamaSchema>,
-): ConvertedSchema {
-  const components: Record<string, OpenApi.IJsonSchema> = {};
-  const result = LlmSchemaComposer.invert("llama")({
-    schema,
-    $defs,
-    components,
-  });
-
-  return {
-    schema: result,
-    components,
-  };
-}
-
 export function convertSchemaFromLlmSchemaV3(
   schema: ILlmSchemaV3,
 ): ConvertedSchema {
@@ -124,22 +98,5 @@ export function convertSchemaFromLlmSchemaV3(
   return {
     schema: result,
     components: {},
-  };
-}
-
-export function convertSchemaFromLlmSchemaV3_1(
-  schema: ILlmSchemaV3_1,
-  $defs: Record<string, ILlmSchemaV3_1>,
-): ConvertedSchema {
-  const components: Record<string, OpenApi.IJsonSchema> = {};
-  const result = LlmSchemaComposer.invert("3.1")({
-    schema,
-    $defs,
-    components,
-  });
-
-  return {
-    schema: result,
-    components,
   };
 }
